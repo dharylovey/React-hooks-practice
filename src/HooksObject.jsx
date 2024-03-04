698e4b3f9973241e9d7383c9b615b8bb8aafdc96
Dharyl Carry S. Almora <37977733+dharylovey@users.noreply.github.com>
2024-03-04 19:48:58 +0800

    useState hook using object

---
+++ src/HooksObject.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const HooksObject = () => {
+  const [name, setName] = useState({
+    name: '',
+    lastName: ''
+  })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HooksObject
